32158533c32091879fbc666c10c04077562c9ef3
Nico Ulbricht <nico.ulbricht@live.de>
2017-11-07 11:14:36 +0100

    redo todolist

--- 07-todo.py
+++ 07-todo.py
@@ -1,2 +1,25 @@
-def addition(number1, number2):
-	
+todos = []
+
+def addTodo(todo):
+    todos.append(todo)
+
+def removeTodo(todo):
+    todos.remove(todo)
+
+def listTodos(todo):
+    print(todos)
+
+while True:
+    command = input('What do you wanna do? ')
+    if command == 'add':
+        todo = input('What to add? ')
+        addTodo(todo)
+    elif command == 'remove':
+        todo = input('What to remove? ')
+        removeTodo(todo)
+    elif command == 'list':
+        listTodos()
+    elif command == 'exit':
+        break
+    else:
+        print('Unknown Command')
